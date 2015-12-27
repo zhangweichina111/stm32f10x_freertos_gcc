ef0fe3a3f05ff3fda7304bd7fe64da25cf16b8d5
zhangwei <zhangweichina111@gmail.com>
2015-12-27 17:16:28 +0000

    modify port of led

--- src/led.h
+++ src/led.h
@@ -5,14 +5,14 @@
 #include "stm32f10x_rcc.h"
 #include "stm32f10x_gpio.h"
 
-#define LED0_RCC	RCC_APB2Periph_GPIOD 
-#define LED1_RCC	RCC_APB2Periph_GPIOG 
+#define LED0_RCC	RCC_APB2Periph_GPIOF 
+#define LED1_RCC	RCC_APB2Periph_GPIOF 
 
-#define LED0_PORT	GPIOD
-#define LED1_PORT	GPIOG
+#define LED0_PORT	GPIOF
+#define LED1_PORT	GPIOF
 
-#define LED0_PIN	GPIO_Pin_13
-#define LED1_PIN	GPIO_Pin_14
+#define LED0_PIN	GPIO_Pin_6
+#define LED1_PIN	GPIO_Pin_7
 
 void led_init(void);
 void led_on(GPIO_TypeDef* port, uint16_t pin);
